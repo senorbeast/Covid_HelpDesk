eb223960b3bb501cd35de0c3b8afe9fb226c23fa
talashilkarraj <2018.raj.talashilkar@ves.ac.in>
2021-04-30 14:57:18 +0530

    Debug Turned to False

--- Covid_HelpDesk/settings.py
+++ Covid_HelpDesk/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2(n^*03-c8atnvx%crt^umajvzfn30g$te(u4fl+ws#ctf^lf0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'covidhub.co.in']
 
 
 # Application definition
